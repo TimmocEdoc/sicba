e2ecc35d8f6eeae93a743b2411a147ad7b06efd5
TimmocEdoc <43539006+TimmocEdoc@users.noreply.github.com>
2018-09-28 21:19:44 +0700

    Add files via upload

---
+++ basic/basic/cmake-build-debug/CMakeFiles/basic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic.dir/main.c.obj"
+  "basic.pdb"
+  "basic.exe"
+  "basic.exe.manifest"
+  "libbasic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
